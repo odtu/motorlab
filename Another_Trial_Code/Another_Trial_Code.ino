2e462691fe71ddd693eaaa76696e8a1c7f48911c
MelisaSarici <sarici.asena@gmail.com>
2017-03-11 01:15:21 +0200

    Another Trial Code
    I found a code which creates pwm inside a loop and counts as well, I
    dont know how it works, I leave it to Hande to try:)) Good night:D

---
+++ Another_Trial_Code/Another_Trial_Code.ino
@@ -0,0 +1,64 @@
+//defining Pins names for the code
+
+int pwm=13; int forward=12; int reverse=11;
+
+
+int encoderValue=0; void count(void); void setup()
+
+{
+
+Serial.begin(9600);
+
+pinMode(21,INPUT);
+
+attachInterrupt(2,count,FALLING);
+
+encoderValue=0;
+
+}
+
+void loop()
+
+{
+
+digitalWrite(pwm,1);
+
+digitalWrite(forward,0);
+
+analogWrite(reverse,140);
+
+Serial.print("Starting\n");
+
+delay(3000); Serial.print("Encoder Value="); Serial.println(encoderValue); while(1)
+
+{
+ 
+
+
+// to print encoder value on the screen Serial.print("Encoder Value="); Serial.println(encoderValue); //Setting value of encoder
+
+//defining the while statement condition if(encoderValue<5000)
+
+break; // loop will break as soon as encoder value reaches 5000 or above digitalWrite(forward,1);
+
+digitalWrite(reverse,0);
+
+analogWrite(pwm,150);
+
+}
+
+digitalWrite(forward,1);
+
+digitalWrite(reverse,1);
+
+analogWrite(pwm,255);
+
+}
+
+void count()
+
+{
+
+encoderValue++;
+
+}
