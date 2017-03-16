67cd8abcaac9fde384bb4394cc5a76163e3a84f9
MelisaSarici <sarici.asena@gmail.com>
2017-03-17 00:32:15 +0200

    Encoder Reading Successful
    I changed the code accordingly and the encoder reading is successful. I
    see some numbers printed in the port (it takes some time for them to
    appear, though) I am not so sure about encoder readers timing and Ts.
    What I mean is that I dont know whether its clock is the same as arduino
    or some other timing itself. For speed calculation,I need the value Ts.
    I couldnt try more because at the most exciting part, my 9V supply died.
    :laughing:

--- pwm_simple/pwm_simple.ino
+++ pwm_simple/pwm_simple.ino
@@ -1,39 +1,79 @@
 
-int encoderValue=0;
 
+int encoder0Pos=0;
 void setup()
 {
-  
-  pinMode(11, OUTPUT);
-  TCCR2A = _BV(COM2A1)  | _BV(WGM21)|_BV(WGM21)|_BV(WGM20) ;
-  
-  OCR2A = 180;
-  
+ pinMode(3, OUTPUT);
+ pinMode(11, OUTPUT);
+ pinMode (2,INPUT);
+ pinMode (4,INPUT);
+   
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS22);
+  OCR2A = 250;
+  OCR2B = 0;
+ 
+   attachInterrupt(0, doEncoderA, CHANGE);
+   attachInterrupt(1, doEncoderB, CHANGE);  
+  Serial.begin (9600);
+  Serial.println (encoder0Pos, DEC);
 }
  
-void count(void); // code for counting the increasing values of encoder ticks void setup()
 
-{
 
-Serial.begin(9600);
-
-pinMode(3,INPUT);
-
-attachInterrupt(2,count,FALLING);
-
-encoderValue=0;
-
-}
  void loop() { 
-   Serial.print("Starting\n");
-   
+  
  } 
 
+ 
+void doEncoderA(){
 
- void count()
-
-{
-
-encoderValue++;
-
+  // look for a low-to-high on channel A
+  if (digitalRead(2) == HIGH) { 
+    // check channel B to see which way encoder is turning
+    if (digitalRead(4) == LOW) {  
+      encoder0Pos = encoder0Pos + 1;         // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;         // CCW
+    }
+  }
+  else   // must be a high-to-low edge on channel A                                       
+  { 
+    // check channel B to see which way encoder is turning  
+    if (digitalRead(4) == HIGH) {   
+      encoder0Pos = encoder0Pos + 1;          // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;          // CCW
+    }
+  }
+  Serial.println (encoder0Pos, DEC);          
+  // use for debugging - remember to comment out
 }
+
+void doEncoderB(){
+
+  // look for a low-to-high on channel B
+  if (digitalRead(4) == HIGH) {   
+   // check channel A to see which way encoder is turning
+    if (digitalRead(2) == HIGH) {  
+      encoder0Pos = encoder0Pos + 1;         // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;         // CCW
+    }
+  }
+  // Look for a high-to-low on channel B
+  else { 
+    // check channel B to see which way encoder is turning  
+    if (digitalRead(2) == LOW) {   
+      encoder0Pos = encoder0Pos + 1;          // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;          // CCW
+    }
+  }
+}
+
+
