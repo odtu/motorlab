ea06bd0346ccb5cbef07b7bb6aa96ac2f8042b4f
MelisaSarici <sarici.asena@gmail.com>
2017-03-18 22:31:11 +0200

    few changes
    There is an issue about what I read. I can not use 2 encoders since I
    use interrupt. I try to understand how interrupt works

--- pwm_simple/pwm_simple.ino
+++ pwm_simple/pwm_simple.ino
@@ -1,22 +1,23 @@
 
 
 int encoder0Pos=0;
+
 void setup()
 {
+  Serial.begin(9600);
+  
  pinMode(3, OUTPUT);
  pinMode(11, OUTPUT);
  pinMode (2,INPUT);
  pinMode (4,INPUT);
-   
-  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+
+ TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
   TCCR2B = _BV(CS22);
-  OCR2A = 250;
+  OCR2A =250;
   OCR2B = 0;
+
+  attachInterrupt(digitalPinToInterrupt(2), doEncoderB, CHANGE);  
  
-   attachInterrupt(0, doEncoderA, CHANGE);
-   attachInterrupt(1, doEncoderB, CHANGE);  
-  Serial.begin (9600);
-  Serial.println (encoder0Pos, DEC);
 }
  
 
@@ -25,35 +26,9 @@ void setup()
   
  } 
 
- 
-void doEncoderA(){
-
-  // look for a low-to-high on channel A
-  if (digitalRead(2) == HIGH) { 
-    // check channel B to see which way encoder is turning
-    if (digitalRead(4) == LOW) {  
-      encoder0Pos = encoder0Pos + 1;         // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;         // CCW
-    }
-  }
-  else   // must be a high-to-low edge on channel A                                       
-  { 
-    // check channel B to see which way encoder is turning  
-    if (digitalRead(4) == HIGH) {   
-      encoder0Pos = encoder0Pos + 1;          // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;          // CCW
-    }
-  }
-  Serial.println (encoder0Pos, DEC);          
-  // use for debugging - remember to comment out
-}
 
 void doEncoderB(){
-
+  
   // look for a low-to-high on channel B
   if (digitalRead(4) == HIGH) {   
    // check channel A to see which way encoder is turning
@@ -74,6 +49,8 @@ void doEncoderB(){
       encoder0Pos = encoder0Pos - 1;          // CCW
     }
   }
+  Serial.println (encoder0Pos, DEC);
+   
 }
 
 
