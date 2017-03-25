bbc9c7fc80b98244b0dbd5915f5a2cfd2dfe6de1
MelisaSarici <sarici.asena@gmail.com>
2017-03-25 21:28:24 +0200

    Empty Script
    Must look like this. Though I don't know why class names did not change
    color as they were shown in the document.

--- pwm_simple/pwm_simple.ino
+++ pwm_simple/pwm_simple.ino
@@ -1,56 +1,39 @@
-
-
-int encoder0Pos=0;
-
-void setup()
-{
-  Serial.begin(9600);
+class Encoder{
   
- pinMode(3, OUTPUT);
- pinMode(11, OUTPUT);
- pinMode (2,INPUT);
- pinMode (4,INPUT);
+  public:
+  Encoder(){}
 
- TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(CS22);
-  OCR2A =250;
-  OCR2B = 0;
-
-  attachInterrupt(digitalPinToInterrupt(2), doEncoderB, CHANGE);  
- 
-}
- 
-
-
- void loop() { 
+  void Update()
+  {}
   
- } 
+};
 
 
-void doEncoderB(){
-  
-  // look for a low-to-high on channel B
-  if (digitalRead(4) == HIGH) {   
-   // check channel A to see which way encoder is turning
-    if (digitalRead(2) == HIGH) {  
-      encoder0Pos = encoder0Pos + 1;         // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;         // CCW
+class Motor {
+  public:
+  Motor(){}
+
+  void update()
+  {}
+    
+    };
+    
+
+
+Encoder encoderA();
+Encoder encoderB();
+Motor motorRun();
+
+
+void setup(){
+    Serial.begin(9600);
+    
+    
     }
-  }
-  // Look for a high-to-low on channel B
-  else { 
-    // check channel B to see which way encoder is turning  
-    if (digitalRead(2) == LOW) {   
-      encoder0Pos = encoder0Pos + 1;          // CW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;          // CCW
-    }
-  }
-  Serial.println (encoder0Pos, DEC);
-   
-}
 
-
+void loop(){
+  encoderA.update();
+  encoderB.update();
+  motorRun();
+      
+      }
