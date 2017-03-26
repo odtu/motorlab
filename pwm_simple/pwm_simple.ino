95cc54e9d2f7ac667de2d66f24097168911289f8
MelisaSarici <sarici.asena@gmail.com>
2017-03-26 21:15:01 +0200

    Running motor with class with arduino
    Motor runs. Kind of reading the encoder, kind of..

--- pwm_simple/pwm_simple.ino
+++ pwm_simple/pwm_simple.ino
@@ -1,39 +1,97 @@
+
+
+
+
 class Encoder{
+  int pin;
+  int value;
+  long runtime;
   
   public:
-  Encoder(){}
+  Encoder(int pin, long runtime ){
+    pinMode(pin,INPUT);
+    
+   
+    }
 
   void Update()
-  {}
-  
-};
+  {
+    unsigned long currenttime=millis();
+    int value=0;
+    if (currenttime<=runtime)
+    {
+    value += digitalRead(pin);
+   
+    Serial.println(value,DEC);
+    }
+    
+    }
+    };
 
 
-class Motor {
+class Motor 
+{
+  int pinMotor;
+int pinState;
+long ontime;
+long offtime;
+unsigned long lasttime;
+long runtime;
+
   public:
-  Motor(){}
+  Motor(int pin,long on, long off)
+  {
+    pinMotor=pin;
+    lasttime=0;
+    pinMode(pinMotor, OUTPUT);
+    ontime=on;
+    offtime=off;
+    pinState=LOW;
+    
+    }
 
-  void update()
-  {}
+  void Update()
+  { 
+    unsigned long currenttime=millis();
+    
+    if ((pinState==HIGH)&&(currenttime-lasttime>=ontime))
+    {pinState=LOW;
+    lasttime=currenttime;
+    digitalWrite(pinMotor,pinState);
+      }
+      else if ((pinState==LOW)&&(currenttime-lasttime>=ontime))
+      {pinState=HIGH;
+      lasttime=currenttime;
+      digitalWrite(pinMotor,pinState);
+        }
+        
+    }
     
     };
     
 
 
-Encoder encoderA();
-Encoder encoderB();
-Motor motorRun();
+Encoder encoderA(2, 100);
+Encoder encoderB(3, 100);
+Motor motorRun(12,4,6);
 
 
 void setup(){
-    Serial.begin(9600);
-    
+  Serial.begin(9600);
     
     }
 
 void loop(){
-  encoderA.update();
-  encoderB.update();
-  motorRun();
+  encoderA.Update();
+  encoderB.Update();
+  motorRun.Update();
+  /*int pinState;
+pinMode(12, OUTPUT);
+unsigned long  Now=millis();
+  if (Now==1000)
+        {
+          pinState=LOW;
+        digitalWrite(12,pinState);
+        }*/
       
       }
